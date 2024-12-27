24c9f6dae12f1b946b912ad03fdcff815257fea6
codywahl <codywahl@gmail.com>
2024-12-27 14:19:13 +0900

    Implement leetcode 11

---
+++ app/src/main/java/neetcode150java/MaxWaterContainer.java
@@ -0,0 +1,62 @@
+package neetcode150java;
+
+/**
+ * Container With Most Water
+ * LeetCode 11
+ * 
+ * You are given an integer array heights where heights[i] represents the height
+ * of the ith bar.
+ * 
+ * You may choose any two bars to form a container. Return the maximum amount of
+ * water a container can store.
+ * 
+ * Example 1:
+ * 
+ * Input: height = [1,7,2,5,4,7,3,6]
+ * 
+ * Output: 36
+ * Example 2:
+ * 
+ * Input: height = [2,2,2]
+ * 
+ * Output: 4
+ * Constraints:
+ * 
+ * 2 <= height.length <= 1000
+ * 0 <= height[i] <= 1000
+ */
+
+/**
+ * 
+ * Explanation:
+ * 
+ */
+public class MaxWaterContainer {
+    public int maxArea(int[] heights) {
+        if (heights == null || heights.length < 2) {
+            return 0;
+        }
+
+        int max = 0;
+        int left = 0;
+        int right = heights.length - 1;
+
+        do {
+            int leftHeight = heights[left];
+            int rightHeight = heights[right];
+            int minHeight = Math.min(leftHeight, rightHeight);
+            int width = right - left;
+            int area = width * minHeight;
+
+            max = Math.max(max, area);
+
+            if (leftHeight < rightHeight) {
+                left++;
+            } else {
+                right--;
+            }
+        } while (left < right);
+
+        return max;
+    }
+}
---
+++ app/src/test/java/neetcode150java/MaxWaterContainerTest.java
@@ -0,0 +1,25 @@
+package neetcode150java;
+
+import org.junit.jupiter.api.Test;
+
+public class MaxWaterContainerTest {
+    @Test
+    public void shouldPassTestCase1() {
+        MaxWaterContainer maxWaterContainer = new MaxWaterContainer();
+        int[] input = { 1, 7, 2, 5, 4, 7, 3, 6 };
+        int expected = 36;
+        int result = maxWaterContainer.maxArea(input);
+
+        assert result == expected;
+    }
+
+    @Test
+    public void shouldPassTestCase2() {
+        MaxWaterContainer maxWaterContainer = new MaxWaterContainer();
+        int[] input = { 2, 2, 2 };
+        int expected = 4;
+        int result = maxWaterContainer.maxArea(input);
+
+        assert result == expected;
+    }
+}
