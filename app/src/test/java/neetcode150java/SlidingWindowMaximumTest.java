ac4640cd6488ced22be6c959f09a0f9bfbcd2161
codywahl <codywahl@gmail.com>
2025-01-16 14:24:50 +0900

    add test

---
+++ app/src/test/java/neetcode150java/SlidingWindowMaximumTest.java
@@ -0,0 +1,20 @@
+package neetcode150java;
+
+import org.junit.jupiter.api.Test;
+
+public class SlidingWindowMaximumTest {
+    @Test
+    public void ShouldPassTestCase1() {
+        SlidingWindowMaximum slidingWindowMaximum = new SlidingWindowMaximum();
+        int[] nums = { 1, 2, 1, 0, 4, 2, 6 };
+        int k = 3;
+        int[] result = slidingWindowMaximum.maxSlidingWindow(nums, k);
+        int[] expected = { 2, 2, 4, 4, 6 };
+
+        assert (result.length == expected.length);
+
+        for (int i = 0; i < result.length; i++) {
+            assert (result[i] == expected[i]);
+        }
+    }
+}
